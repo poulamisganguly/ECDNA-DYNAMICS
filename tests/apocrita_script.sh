4fac70a2314965eec5f9ee4ad4ec63b1078f7661
Poulami Somanya Ganguly <ahw899@frontend11.apocrita>
2023-04-24 15:03:30 +0100

    Add array job to apocrita script

--- tests/apocrita_script.sh
+++ tests/apocrita_script.sh
@@ -4,8 +4,9 @@
 #$ -l h_rt=1:0:0
 #$ -l h_vmem=1G
 #$ -N ecDNA_evolution
+#$ -t 1-4
 
-cd ../src
-make clean
-make 
-./ecDNA_evolution_2types.exe
+INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" list_of_args.txt)
+make -C /data/home/ahw899/ECDNA-DYNAMICS/src clean
+make -C /data/home/ahw899/ECDNA-DYNAMICS/src
+/data/home/ahw899/ECDNA-DYNAMICS/src/ecDNA_evolution_2types.exe $INPUT_ARGS
